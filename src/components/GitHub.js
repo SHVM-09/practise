51a46188f5f47aa2cf2ac9f06c4a031f9bd3f824
shivam upadhyay <alienc512345@gmial.com>
2022-11-17 15:15:19 +0530

    added video

--- src/components/GitHub.js
+++ src/components/GitHub.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
 import "./GitHub.css";
 export default function GitHub() {
   return (
-    <h1 className='h1'>First GitHub Deployment</h1>
-  )
+    <>
+      <h1 className="h1">First GitHub Deployment</h1>
+      <iframe
+        src="https://www.youtube.com/embed/watch?v=eJnQBXmZ7Ek&list=RDGMEM6ijAnFTG9nX1G-kbWBUCJAVMeJnQBXmZ7Ek&start_radio=1"
+        title="youtube"
+        height={"400px"}
+        width={"600px"}
+      ></iframe>
+    </>
+  );
 }
